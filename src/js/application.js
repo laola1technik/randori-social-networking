997ad3dfe0be1ede1920eebf421c077c70748562
laola1technik <laola1technik@users.noreply.github.com>
2017-10-17 14:24:06 +0200

    Adds project structure with webpack application entry point.

---
+++ src/js/application.js
@@ -0,0 +1,9 @@
+/**
+ * Setup module. Wire together components, inject dependencies, start application.
+ */
+
+const onLoad = function () {
+    alert('loaded');
+};
+
+document.addEventListener("DOMContentLoaded", onLoad);
