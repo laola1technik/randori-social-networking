d2eefb14beccb20044b0cda3e94b2979cf64a212
laola1technik <laola1technik@users.noreply.github.com>
2017-10-31 15:43:29 +0100

    Format.

--- test/socialNetworking_test.js
+++ test/socialNetworking_test.js
@@ -1,13 +1,14 @@
 /* global describe, it */
 const should = require('chai').should();
-const SocialNetworking = require('../src/js/socialNetworking');
+const SocialNetworking = require('socialNetworking.js');
 
 describe('Social Networking', function () {
     it('should publish message', function () {
         const socialNetworking = new SocialNetworking();
-        socialNetworking.submit('Alice -> I love the weather today');
-        const timeline = socialNetworking.submit('Alice');
 
-        timeline.should.equal('I love the weather today (1 second ago)');
+        socialNetworking.submit('Alice -> I love the weather today');
+
+        const timeLine = socialNetworking.submit('Alice');
+        timeLine.should.equal('I love the weather today (1 second ago)');
     });
 });
