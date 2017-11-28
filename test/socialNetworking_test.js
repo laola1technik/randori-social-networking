fcb2f6c0eb184070c3cec213ca12ed9d1519ac3b
laola1technik <laola1technik@users.noreply.github.com>
2017-11-28 15:28:28 +0100

    TODOs.

--- test/socialNetworking_test.js
+++ test/socialNetworking_test.js
@@ -103,3 +103,4 @@ describe('Social Networking', function () {
     });
 });
 //TODO: Only see messages after follows command in wall
+//TODO: typical problematic inputs like quote, single char, large input, etc...
