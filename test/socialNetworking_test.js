2cc5fd11f293cd6cd5657b72beb7c280fabbc1a0
laola1technik <laola1technik@users.noreply.github.com>
2017-11-28 16:55:59 +0100

    Sketch next test.

--- test/socialNetworking_test.js
+++ test/socialNetworking_test.js
@@ -122,6 +122,26 @@ describe('Social Networking', function () {
             wall.should.equal('Daniel - The weather is great! (1 second ago)\n'
                 + 'Bob - The weather is nice today! (1 second ago)');
         });
+
+        //it('should display message of all followed users in right order', function (done) {
+        //    const socialNetworking = new SocialNetworking();
+        //    socialNetworking.submit('Jim follows Bob');
+        //    socialNetworking.submit('Jim follows Daniel');
+        //    socialNetworking.submit('Bob -> The weather is nice today!');
+        //    setTimeout(function () {
+        //        socialNetworking.submit('Daniel -> The weather is great!');
+        //        setTimeout(function () {
+        //            socialNetworking.submit('Bob -> Indeed!');
+        //
+        //            const wall = socialNetworking.submit('Jim wall');
+        //
+        //            wall.should.equal('Bob - Indeed! (1 second ago)\n' +
+        //                'Daniel - The weather is great! (2 seconds ago)\n' +
+        //                'Bob - The weather is nice today! (3 seconds ago)');
+        //            done();
+        //        }, 1000);
+        //    }, 1000);
+        //});
     });
 });
 //TODO: Only see messages after follows command in wall
