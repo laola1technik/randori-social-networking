fa6089061aa8b674909c7949572461b5e7bec1fb
laola1technik <laola1technik@users.noreply.github.com>
2018-02-16 15:27:16 +0100

    Removes unused application file.

--- src/js/application.js
+++
@@ -1,9 +0,0 @@
-/**
- * Setup module. Wire together components, inject dependencies, start application.
- */
-
-const onLoad = function () {
-    alert('loaded');
-};
-
-document.addEventListener("DOMContentLoaded", onLoad);
